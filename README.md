4515a670dc5687a789560ab161a0d20730979aa3
Kien085 <ktn085@ucsd.edu>
2021-07-29 03:28:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# SG2Caps
+SG2Caps: In Defense of Scene Graphs for Image Captioning, ICCV 2021
+
+# Acknowledgement
+This code is implemented based on Ruotian Luo's implementation of image captioning in https://github.com/ruotianluo/self-critical.pytorch.
+
+The training code is implemented based on Yang Xu's implementation of SGAE in https://github.com/yangxuntu/SGAE.
+
+# Installation anaconda and the environment
+Follow the instructions to set up the environment/data from Yang Xu's README.
+
+# Additional Data
+In addition, download cocobu2_append.json and the all_ISG folder, and place them into the data folder.
+
+# Training the model
+1. After setting up the SGAE environment, and downloading the additional data:
+```
+python train_mem.py --id id08 --caption_model lstm_mem4 --input_json data/cocobu2_appended.json --input_fc_dir data/cocobu_fc --input_att_dir data/cocobu_att --input_rela_dir data/cocobu_img_sg --input_ssg_dir data/all_ISG --input_label_h5 data/cocobu2_label.h5 --sg_dict_path data/spice_sg_dict2.npz --batch_size 50 --accumulate_number 2 --learning_rate 5e-4 --learning_rate_decay_start 0 --learning_rate_decay_every 3 --scheduled_sampling_start 0 --checkpoint_path storedData/id08 --save_checkpoint_every 3000 --val_images_use 5000 --max_epochs 150 --rnn_size 1000 --input_encoding_size 1000 --att_feat_size 2048 --att_hid_size 512 --self_critical_after 20 --train_split train --memory_size 10000 --memory_index c --step2_train_after 100 --step3_train_after 200 --use_rela 0 --gpu 0
+```
+
+2.The Google Drive also contains a pre-trained model that we used to get the numbers reported in the paper, under id13.
+
+3.The details of parameters:
+
+--id: the id of your model, which is usually set as the same as check point, which is helpful for you to train from the check point.
+
+--batch_size, --accumulate_number: these two parameters are set for users who do not have large gpu, if you want to set batch size as 100, you can set batch_size as 50, and set accumulate_number as 2, also you can set batch_size as 20 and accumulate_number as 5. Importantly, they are not totally equal to set batch_size as 100 and accumulate_number as 1, the bigger the bathc_size is, the higher the performance.
+
+--self_critical_after: when reinforcement leanring begins, if this value is set as 40, it means that after training 40 epoches, the reinforcement loss is used. Generally, if you want to have a good CIDEr-D score, you should use cross entropy loss first and then use reinforcement loss.
+
+--checkpoint_path: this is where the models will be saved. Create the folders before starting the training.
+
+4.Tranining from checkpoints.
+In order to continue training from a checkpoint, just add a start_from parameter to the end of the training code.
+The codes provide the ability of training from checkpoints. For example, if you want to train the model from one checkpoint, say, 22, you can use the following code to continute:
+```
+python train_mem.py --id id08 --caption_model lstm_mem4 --input_json data/cocobu2_appended.json --input_fc_dir data/cocobu_fc --input_att_dir data/cocobu_att --input_rela_dir data/cocobu_img_sg --input_ssg_dir data/all_ISG --input_label_h5 data/cocobu2_label.h5 --sg_dict_path data/spice_sg_dict2.npz --batch_size 50 --accumulate_number 2 --learning_rate 5e-4 --learning_rate_decay_start 0 --learning_rate_decay_every 3 --scheduled_sampling_start 0 --checkpoint_path storedData/id08 --save_checkpoint_every 3000 --val_images_use 5000 --max_epochs 150 --rnn_size 1000 --input_encoding_size 1000 --att_feat_size 2048 --att_hid_size 512 --self_critical_after 20 --train_split train --memory_size 10000 --memory_index c --step2_train_after 100 --step3_train_after 200 --use_rela 0 --gpu 0 --start_from 16
+```
+
+# Evaluating the model
+1.To evaluate your code, run the following:
+```
+python eval_mem.py --dump_images 0 --num_images 5000 --model storedData/id06/modelid060026.pth --infos_path storedData/id06/infos_id060026.pkl --language_eval 1 --beam_size 1 --split test --index_eval 1 --use_rela 0 --training_mode 0 --memory_cell_path storedData/id06/memory_cellid060026.npz --sg_dict_path data/spice_sg_dict2.npz --input_ssg_dir data/all_ISG
+```
+Make sure that model, infos_path, and memory_cell_path are pointing to the correct files and folders.
+
+# Creating Scene Graph Visualizations
+The two python scripts inside of the vis folder will create the scene graph visualizations from the input_ssg_dir that is normally used to train the model (all_ISG, HOI_ISG). You will need to change the folder variable to point to the correct folder, depending on the SSG.
+
+To create only one SSG for one image. The resulting image will be saved in the ./data folder
+```
+python test.py --mode sen --id 391895
+```
+
+For all images, run vis_sg.py. The resulting images will be saved in the ./data/vis folder. Make sure this folder exists before running.
---
+++ vis/vis_sg.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#Modified by Kien Nguyen to print out some scene graphs he wanted to print
+
+import tempfile, os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from imageio import imread
+
+import argparse
+import numpy as np
+
+
+"""
+Utilities for making visualizations.
+"""
+
+
+def draw_scene_graph(objs, triples, attributes, vocab=None, **kwargs):
+    """
+    Use GraphViz to draw a scene graph. If vocab is not passed then we assume
+    that objs and triples are python lists containing strings for object and
+    relationship names.
+    Using this requires that GraphViz is installed. On Ubuntu 16.04 this is easy:
+    sudo apt-get install graphviz
+    """
+    output_filename = kwargs.pop('output_filename', 'graph.png')
+    orientation = kwargs.pop('orientation', 'V')
+    edge_width = kwargs.pop('edge_width', 6)
+    arrow_size = kwargs.pop('arrow_size', 1.5)
+    binary_edge_weight = kwargs.pop('binary_edge_weight', 1.2)
+    ignore_dummies = kwargs.pop('ignore_dummies', True)
+  
+    if orientation not in ['V', 'H']:
+        raise ValueError('Invalid orientation "%s"' % orientation)
+    rankdir = {'H': 'LR', 'V': 'TD'}[orientation]
+
+    if vocab is not None:
+    # Decode object and relationship names
+        assert torch.is_tensor(objs)
+        assert torch.is_tensor(triples)
+        objs_list, triples_list = [], []
+        for i in range(objs.size(0)):
+            objs_list.append(vocab['object_idx_to_name'][objs[i].item()])
+        for i in range(triples.size(0)):
+            s = triples[i, 0].item()
+            p = vocab['pred_name_to_idx'][triples[i, 1].item()]
+            o = triples[i, 2].item()
+            triples_list.append([s, p, o])
+        objs, triples = objs_list, triples_list
+
+    # General setup, and style for object nodes
+    lines = [
+        'digraph{',
+        'graph [size="5,3",ratio="compress",dpi="300",bgcolor="transparent"]',
+        'rankdir=%s' % rankdir,
+        'nodesep="0.5"',
+        'ranksep="0.5"',
+        'node [shape="box",style="rounded,filled",fontsize="48",color="none"]',
+        'node [fillcolor="lightpink1"]',
+    ]
+    # Output nodes for objects
+    for i, obj in enumerate(objs):
+        if ignore_dummies and obj == '__image__':
+            continue
+        lines.append('%d [label="%s"]' % (i, obj))
+
+    # Output relationships
+    next_node_id = len(objs)
+    lines.append('node [fillcolor="lightblue1"]')
+    for s, p, o in triples:
+        if ignore_dummies and p == '__in_image__':
+            continue
+        lines += [
+            '%d [label="%s"]' % (next_node_id, p),
+            '%d->%d [penwidth=%f,arrowsize=%f,weight=%f]' % (
+              s, next_node_id, edge_width, arrow_size, binary_edge_weight),
+            '%d->%d [penwidth=%f,arrowsize=%f,weight=%f]' % (
+              next_node_id, o, edge_width, arrow_size, binary_edge_weight)
+        ]
+        next_node_id += 1
+
+    #Output Attributes
+    lines.append('node [fillcolor="lightseagreen"]')
+    for a,o in attributes:
+        lines += [
+            '%d [label="%s"]' % (next_node_id, a),
+            '%d->%d [penwidth=%f,arrowsize=%f,weight=%f]' % (
+               o, next_node_id, edge_width, arrow_size, binary_edge_weight)
+        ]
+        next_node_id += 1
+    lines.append('}')
+
+  # Now it gets slightly hacky. Write the graphviz spec to a temporary
+  # text file
+    ff, dot_filename = tempfile.mkstemp()
+    with open(dot_filename, 'w') as f:
+        for line in lines:
+            f.write('%s\n' % line)
+    os.close(ff)
+
+  # Shell out to invoke graphviz; this will save the resulting image to disk,
+  # so we read it, delete it, then return it.
+    output_format = os.path.splitext(output_filename)[1][1:]
+    os.system('dot -T%s %s > %s' % (output_format, dot_filename, output_filename))
+    os.remove(dot_filename)
+    img = imread(output_filename)
+    os.remove(output_filename)
+
+    return img
+
+
+if __name__ == '__main__':
+    sg_dict = np.load('../data/spice_sg_dict2.npz', allow_pickle=True)['spice_dict'][()]
+    sg_dict = sg_dict['ix_to_word']
+    folder = '../data/coco_spice_sg2/'
+    for filename in os.listdir("../data/coco_spice_sg2"):
+        imageId = filename.split(".")[0]
+        objs = []
+        triples = []
+        attributes = []
+        # parser = argparse.ArgumentParser()
+        # # Input paths
+        # parser.add_argument('--id', type=str, default='0',
+        #                     help='id of image')
+        # parser.add_argument('--mode', type=str,  default='sen',
+        #                     help='image or sen')
+        # opt = parser.parse_args()
+        sg_path = folder + imageId + '.npy'
+        sg_use = np.load(sg_path, allow_pickle=True)[()]
+        rela = sg_use['rela_info']
+        obj_attr = sg_use['obj_info']
+        N_rela = len(rela)
+        N_obj = len(obj_attr)
+        for i in range(N_obj):
+            singleObj = sg_dict[obj_attr[i][0]]
+            if len(obj_attr[i]) >= 2:
+                for j in range(len(obj_attr[i])-1):
+                    attr = sg_dict[obj_attr[i][j + 1]]
+                    attributes.append([attr,singleObj])
+            objs.append(singleObj)
+
+        objsEncode = {s: i for i, s in enumerate(objs)}
+
+        encodedAttr = []
+        for a,o in attributes:
+            encodObj = objsEncode[o]
+            encodedAttr.append([a,encodObj])
+
+        for i in range(N_rela):
+            obj_idx = 0
+            sbj = sg_dict[ int(obj_attr[int(rela[i][0])][obj_idx]) ]
+            obj = sg_dict[ int(obj_attr[int(rela[i][1])][obj_idx]) ]
+            rela_name = sg_dict[rela[i][2]]
+            sbj = objsEncode[sbj]
+            obj = objsEncode[obj]
+            triples.append([sbj,rela_name,obj])
+
+        
+        vocab = None
+        image = (plt.imshow(draw_scene_graph(objs, triples, encodedAttr, vocab, orientation='V')))
+        savePath = './data/' + imageId + '.png'
+        print(imageId)
+        plt.savefig(savePath)
+        plt.clf()
